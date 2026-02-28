7d38d3f85f67d996ee665afc02666b89b8267f61
walter.hwang <lyvius2@naver.com>
2026-03-01 00:16:39 +0900

    블로그 글 필드 수정

--- src/main/kotlin/com/walter/lifelog/mapper/PostMapper.kt
+++ src/main/kotlin/com/walter/lifelog/mapper/PostMapper.kt
@@ -3,15 +3,12 @@ package com.walter.lifelog.mapper
 import com.walter.lifelog.controller.dto.PostResponse
 import com.walter.lifelog.entity.Post
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 interface PostMapper {
-    @Mapping(target = "isFeatured", source = "featured")
     fun toDto(post: Post): PostResponse
 
-    @Mapping(target = "isFeatured", source = "featured")
     fun toDtoList(posts: List<Post>): List<PostResponse>
 }
 

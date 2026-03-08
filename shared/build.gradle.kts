c44bfe38409127f7c858ec2e7413925b67fd696b
walter.hwang <lyvius2@naver.com>
2026-03-08 20:38:18 +0900

    Google API Auth 기능 추가

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     api("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
     api("org.mapstruct:mapstruct:$mapstructVersion")
+    api("com.google.api-client:google-api-client:2.7.2")
+    api("com.google.apis:google-api-services-drive:v3-rev20250220-2.0.0")
     implementation("org.commonmark:commonmark:$commonmarkVersion")
     implementation("org.commonmark:commonmark-ext-gfm-tables:$commonmarkVersion")
     implementation("org.commonmark:commonmark-ext-gfm-strikethrough:$commonmarkVersion")
---
+++ shared/src/main/java/com/walter/lifelog/shared/config/GoogleDriveConfig.java
@@ -0,0 +1,47 @@
+package com.walter.lifelog.shared.config;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.DriveScopes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@Configuration
+public class GoogleDriveConfig {
+    public static final String APPLICATION_NAME = "Lifelog Photo Archive";
+    public static final String CREDENTIALS_FILE_PATH = "/credential.json";
+    public static final String TOKENS_DIRECTORY_PATH = "tokens";
+    public static final String USER_ID = "user";
+    public static final String CALLBACK_PATH = "/google-auth/callback";
+
+    @Bean
+    public GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow() throws IOException {
+        final GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        final NetHttpTransport httpTransport = new NetHttpTransport();
+        final InputStream credentialStream = GoogleDriveConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (credentialStream == null) {
+            throw new IllegalStateException("credential.json 파일을 찾을 수 없습니다.");
+        }
+
+        final GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(credentialStream));
+        return new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport,
+                jsonFactory,
+                clientSecrets,
+                List.of(DriveScopes.DRIVE_READONLY)
+        )
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+    }
+}
+

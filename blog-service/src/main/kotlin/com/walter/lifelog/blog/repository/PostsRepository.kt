ed1193309730f84212ed6c89084a3a7d348b21f1
walter.hwang <lyvius2@naver.com>
2026-03-19 10:29:18 +0900

    포스트 발행 상태 누락 디버깅

--- blog-service/src/main/kotlin/com/walter/lifelog/blog/repository/PostsRepository.kt
+++ blog-service/src/main/kotlin/com/walter/lifelog/blog/repository/PostsRepository.kt
@@ -9,9 +9,9 @@ interface PostsRepository : JpaRepository<Post, Long> {
     fun findBySlug(slug: String): Post?
     fun findByPostSeq(id: Long): Post?
 
-    @Query("SELECT p FROM Post p WHERE p.categorySeq = :categorySeq AND p.createdAt < :createdAt ORDER BY p.createdAt DESC LIMIT 1")
+    @Query("SELECT p FROM Post p WHERE p.categorySeq = :categorySeq AND p.status = 'PUBLISHED' AND p.createdAt < :createdAt ORDER BY p.createdAt DESC LIMIT 1")
     fun findPrevPost(categorySeq: Long, createdAt: LocalDateTime): Post?
 
-    @Query("SELECT p FROM Post p WHERE p.categorySeq = :categorySeq AND p.createdAt > :createdAt ORDER BY p.createdAt ASC LIMIT 1")
+    @Query("SELECT p FROM Post p WHERE p.categorySeq = :categorySeq AND p.status = 'PUBLISHED' AND p.createdAt > :createdAt ORDER BY p.createdAt ASC LIMIT 1")
     fun findNextPost(categorySeq: Long, createdAt: LocalDateTime): Post?
 }

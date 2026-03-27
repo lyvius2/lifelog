c680f18f60777362a668a3b6ce65b87ee18646a5
walter.hwang <lyvius2@naver.com>
2026-03-27 22:48:09 +0900

    contents 수정

--- content-service/src/main/kotlin/com/walter/lifelog/content/service/ContentService.kt
+++ content-service/src/main/kotlin/com/walter/lifelog/content/service/ContentService.kt
@@ -4,14 +4,12 @@ import com.walter.lifelog.content.entity.code.ContentType
 import com.walter.lifelog.content.repository.ContentDocumentRepository
 import com.walter.lifelog.shared.annotation.DynamicCacheable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ContentService(
     private val contentDocumentRepository: ContentDocumentRepository,
 ) {
     @DynamicCacheable(value = ["contentByType"], key = "#contentType.name", ttlMinutes = 5)
-    @Transactional(readOnly = true)
     fun getContentByType(contentType: ContentType): HashMap<String, Any> {
         val contentDocument = contentDocumentRepository.findByContentType(contentType)
         return contentDocument?.content ?: HashMap()

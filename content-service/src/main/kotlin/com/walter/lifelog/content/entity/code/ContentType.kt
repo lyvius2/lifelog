1490a589548def253ee8cd777f7744e5e936755c
walter.hwang <lyvius2@naver.com>
2026-03-30 23:27:59 +0900

    ContentType 추가 정의

--- content-service/src/main/kotlin/com/walter/lifelog/content/entity/code/ContentType.kt
+++ content-service/src/main/kotlin/com/walter/lifelog/content/entity/code/ContentType.kt
@@ -3,6 +3,9 @@ package com.walter.lifelog.content.entity.code
 enum class ContentType(val typeDescription: String) {
     INTRO("웹사이트 개요"),
     PROFILE("개발자 자기소개"),
-    CAR("보유 자동차 소개"),
     ARCHITECTURE("프로젝트 아키텍처 소개"),
+    CAR("보유 자동차 소개"),
+    FURAIKI("풍우래기"),
+    JAPAN_100_MOUNTAINS("일본 100명산"),
+    RETRO_GAMES("레트로게임")
 }

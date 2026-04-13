30a96a859f9a90b09e636c3b8fffe6946bb5024f
walter.hwang <lyvius2@naver.com>
2026-04-13 17:00:44 +0900

    MongoDB 컨텐츠 타입 추가

--- content-service/src/main/kotlin/com/walter/lifelog/content/entity/code/ContentType.kt
+++ content-service/src/main/kotlin/com/walter/lifelog/content/entity/code/ContentType.kt
@@ -4,6 +4,7 @@ enum class ContentType(val typeDescription: String) {
     INTRO("웹사이트 개요"),
     PROFILE("개발자 자기소개"),
     ARCHITECTURE("프로젝트 아키텍처 소개"),
+    AI_OPS_DESIGN("Spring AI Ops 아키텍처 소개"),
     CAR("보유 자동차 소개"),
     FURAIKI("풍우래기"),
     JAPAN_100_MOUNTAINS("일본 100명산"),

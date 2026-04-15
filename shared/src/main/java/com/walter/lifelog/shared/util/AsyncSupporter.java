2a215d58bf14f5103f3fdafa3255b690a2bfd516
lyvius2 <walter.hwang@icloud.com>
2026-04-15 17:49:14 +0900

    virtual thread 관련 보완

--- shared/src/main/java/com/walter/lifelog/shared/util/AsyncSupporter.java
+++ shared/src/main/java/com/walter/lifelog/shared/util/AsyncSupporter.java
@@ -1,14 +1,22 @@
 package com.walter.lifelog.shared.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.task.TaskExecutor;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 public final class AsyncSupporter {
+    private static final Logger log = LoggerFactory.getLogger(AsyncSupporter.class);
+
     private AsyncSupporter() {}
 
     public static <T> CompletableFuture<T> asyncSupply(TaskExecutor executor, Supplier<T> supplier) {
-        return CompletableFuture.supplyAsync(supplier, executor);
+        return CompletableFuture.supplyAsync(supplier, executor)
+                .exceptionally(ex -> {
+                    log.error("Async task failed", ex);
+                    return null;
+                });
     }
 }
 
